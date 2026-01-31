4de75b571b52356d2a9d97df7ebe050f32d765b8
Pedro Viana de Castilhos <pedro.viana.castilhos@gmail.com>
2026-01-31 20:06:39 -0300

    :sparkles: feat: Adicionando os endpoints de consulta de filmes

--- app/main.py
+++ app/main.py
@@ -3,6 +3,21 @@ from hishel.httpx import AsyncCacheClient
 
 app = FastAPI()
 
+# Endpoint raiz
 @app.get("/")
 async def root():
     return {"greetings": "May the Force be with you!"}
+
+# Endpoint para solicitar dados de todos os filmes
+@app.get("/films")
+async def get_films():
+    async with AsyncCacheClient() as client:
+        response = await client.get("https://swapi.dev/api/films/")
+    return response.json()
+
+# Endpoint para solicitar dados de um filme específico pelo ID
+@app.get("/films/{film_id}")
+async def get_film(film_id: int):
+    async with AsyncCacheClient() as client:
+        response = await client.get(f"https://swapi.dev/api/films/{film_id}/")
+    return response.json()
