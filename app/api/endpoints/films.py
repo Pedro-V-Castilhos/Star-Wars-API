a3623fc0f2fbea7a84911e6bac5aa79389d2b2f3
Pedro Viana de Castilhos <pedro.viana.castilhos@gmail.com>
2026-02-01 10:46:48 -0300

    :sparkles: feat: Adicionando a busca de planetas, naves, veículos e espécies em um filme específico

--- app/api/endpoints/films.py
+++ app/api/endpoints/films.py
@@ -17,3 +17,27 @@ async def get_film(film_id: int, request: Request):
 async def get_film_characters(film_id: int, request: Request):
     responses = await helpers.get_all_from_url(f"https://swapi.dev/api/films/{film_id}/", "characters", request)
     return {"results": responses}
+
+# Endpoint para solicitar dados dos planetas de um filme específico pelo ID
+@router.get("/{film_id}/planets", tags=["Planets"])
+async def get_film_planets(film_id: int, request: Request):
+    responses = await helpers.get_all_from_url(f"https://swapi.dev/api/films/{film_id}/", "planets", request)
+    return {"results": responses}
+
+# Endpoint para solicitar dados das naves espaciais de um filme específico pelo ID
+@router.get("/{film_id}/starships", tags=["Starships"])
+async def get_film_starships(film_id: int, request: Request):
+    responses = await helpers.get_all_from_url(f"https://swapi.dev/api/films/{film_id}/", "starships", request)
+    return {"results": responses}
+
+# Endpoint para solicitar dados dos veículos de um filme específico pelo ID
+@router.get("/{film_id}/vehicles", tags=["Vehicles"])
+async def get_film_vehicles(film_id: int, request: Request):
+    responses = await helpers.get_all_from_url(f"https://swapi.dev/api/films/{film_id}/", "vehicles", request)
+    return {"results": responses}
+
+# Endpoint para solicitar dados das espécies de um filme específico pelo ID
+@router.get("/{film_id}/species", tags=["Species"])
+async def get_film_species(film_id: int, request: Request):
+    responses = await helpers.get_all_from_url(f"https://swapi.dev/api/films/{film_id}/", "species" , request)
+    return {"results": responses}
