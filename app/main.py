9526683a5c4ade9f0a7c4fd8d5a267aaa4c2a291
Pedro Viana de Castilhos <pedro.viana.castilhos@gmail.com>
2026-01-31 22:39:37 -0300

    :sparkles: feat: Adicionando o endpoint de pesquisar personagens de um filme

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from .utils import helpers
 from .config import lifespan
+import asyncio
 
 app = FastAPI(lifespan=lifespan)
 
@@ -17,4 +18,13 @@ async def get_films(request: Request):
 # Endpoint para solicitar dados de um filme específico pelo ID
 @app.get("/films/{film_id}")
 async def get_film(film_id: int, request: Request):
-    return await helpers.get_from_url(f"https://swapi.dev/api/films/{film_id}/", request    )
+    return await helpers.get_from_url(f"https://swapi.dev/api/films/{film_id}/", request)
+
+# Endpoint para solicitar dados dos personagens de um filme específico pelo ID
+@app.get("/films/{film_id}/characters")
+async def get_film_characters(film_id: int, request: Request):
+    film_data = await helpers.get_from_url(f"https://swapi.dev/api/films/{film_id}/", request)
+    character_urls = film_data.get("characters")
+    tasks = [helpers.get_from_url(url, request) for url in character_urls]
+    responses = await asyncio.gather(*tasks)
+    return {"results": responses}
