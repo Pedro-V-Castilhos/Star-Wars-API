269f8bb4d439b3516e1eaf6fc9da77b1c991a20f
Pedro Viana de Castilhos <pedro.viana.castilhos@gmail.com>
2026-02-02 21:50:16 -0300

    :ok_hand: style: Adicionando comentários descrevendo as funções helpers

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -10,11 +10,13 @@ async def get_from_url(url: str, request: Request, order_by: str = "", reverse: 
         data["results"] = await sort_results(data.get("results"), order_by, reverse)
     return data
 
+# Helper para obter todos os itens relacionados de uma URL específica
 async def get_all_from_url(url: str, data: str, request: Request, order_by: str = "", reverse: bool = False):
     film_data = await get_from_url(url, request, order_by, reverse)
     responses = await get_all_from_array(film_data[data], request, order_by, reverse)
     return {"results": responses}
 
+# Helper para obter todos os itens de uma lista de URLs
 async def get_all_from_array(urls: list[str], request: Request, order_by: str = "", reverse: bool = False):
     tasks = [get_from_url(url, request, order_by, reverse) for url in urls]
     responses = await asyncio.gather(*tasks)
@@ -22,6 +24,7 @@ async def get_all_from_array(urls: list[str], request: Request, order_by: str = 
         responses = await sort_results(responses, order_by, reverse)
     return responses
 
+# Helper para obter todos os itens paginados de uma URL base
 async def get_all_from_pages(base_url: str, request: Request, order_by: str = "", reverse: bool = False):
     all_results = []
     url = base_url
@@ -35,6 +38,7 @@ async def get_all_from_pages(base_url: str, request: Request, order_by: str = ""
         all_results = await sort_results(all_results, order_by, reverse)
     return {"count": len(all_results),"pages": pages, "results": all_results}
 
+# Helper para ordenar resultados com base em uma chave específica
 async def sort_results(data: list[dict], order_by: str, is_reverse: bool):
     if order_by:
         if order_by in data[0] and type(data[0][order_by]) in [str, int]:
